9275f30b6fe29cfe800694c0b71ef708a924e57e
Emilio Cartoni <emilio.cartoni@yahoo.it>
2020-04-30 13:00:48 +0200

    Added option to avoid rendering when using the RandomPolicy controller

--- my_controller.py
+++ my_controller.py
@@ -8,9 +8,12 @@ from baseline.baseline import Baseline
 class RandomPolicy(BasePolicy):
     def __init__(self, action_space):
         self.action_space = action_space
+        self.render = False
 
     def step(self, observation, reward, done):
-        return self.action_space.sample()
+        action = self.action_space.sample()
+        action['render'] = self.render
+        return action
 
-
+#SubmittedPolicy=RandomPolicy
 SubmittedPolicy=Baseline
