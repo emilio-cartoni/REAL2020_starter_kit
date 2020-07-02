097f8ed0b6ca1aab16bd373c0bedd146a1712908
Emilio Cartoni <emilio.cartoni@yahoo.it>
2020-07-02 16:18:01 +0200

    Updated config defaults

--- local_evaluation.py
+++ local_evaluation.py
@@ -4,21 +4,19 @@ import real_robots
 from my_controller import SubmittedPolicy
 import os
 
-DATASET_PATH=os.getenv("AICROWD_DATASET_PATH", "./data/goals-REAL2020-sNone-50-1.npy.npz")
+DATASET_PATH=os.getenv("AICROWD_DATASET_PATH", "./data/goals-REAL2020-s2020-50-1.npy.npz")
 
 result, detailed_scores = real_robots.evaluate(
                 SubmittedPolicy,
                 environment='R1',
                 action_type='macro_action',
                 n_objects=1,
-                intrinsic_timesteps=0e6,
+                intrinsic_timesteps=15e6,
                 extrinsic_timesteps=10e3,
                 extrinsic_trials=50,
                 visualize=False,
                 goals_dataset_path=DATASET_PATH
             )
-#  NOTE : You can find a sample goals.npy.npz file at
-#
-#  https://aicrowd-production.s3.eu-central-1.amazonaws.com/misc/REAL-Robots/goals.npy.npz
+
 print(result)
 print(detailed_scores)
