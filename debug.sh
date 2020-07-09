2a86f7dcb249a9909a4400f1d7f74cb2439a03f8
Emilio Cartoni <emilio.cartoni@yahoo.it>
2020-07-09 14:53:55 +0200

    Updated debug.sh

--- debug.sh
+++ debug.sh
@@ -5,20 +5,22 @@
 # Build Docker Image
 
 export IMAGE_NAME="real-robot-random-agent"
+IMAGE_TAG=$(git rev-parse --short HEAD)
 REPO2DOCKER="$(which aicrowd-repo2docker)"
 sudo ${REPO2DOCKER} --no-run \
   --user-id 1001 \
   --user-name aicrowd \
-  --image-name ${IMAGE_NAME} \
+  --image-name ${IMAGE_NAME}:${IMAGE_TAG} \
   --debug .
 
 
 # Run local_evaluation inside docker container
 # The next step assumes that you have downloaded the goals.npy.npz file to the 
 # data folder of your repository
-sudo nvidia-docker run \
+sudo docker run \
+    --gpus all \
     --net=host \
     -v `pwd`/data:/DATASET:z \
-    -e AICROWD_DATASET_PATH=/DATASET/goals.npy.npz \
+    -e AICROWD_DATASET_PATH=/DATASET/goals-REAL2020-s2020-50-1 \
     -it ${IMAGE_NAME}:${IMAGE_TAG} \
     /home/aicrowd/run.sh
