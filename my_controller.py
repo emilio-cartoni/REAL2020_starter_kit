40a270824533cb3fb47ef8b999b718440c82980c
Emilio Cartoni <emilio.cartoni@yahoo.it>
2020-09-15 17:11:19 +0200

    Updated my_controller.py for changes in real_robots BasePolicy and evaluate.py

--- my_controller.py
+++ my_controller.py
@@ -3,8 +3,9 @@ from baseline.policy import Baseline
 
 
 class RandomPolicy(BasePolicy):
-    def __init__(self, action_space):
+    def __init__(self, action_space, observation_space):
         self.action_space = action_space
+        self.observation_space = observation_space
         self.render = False
 
     def step(self, observation, reward, done):
