4309b5c3c13a0526e45ca531bccd6e06bcd5f321
Emilio Cartoni <emilio.cartoni@yahoo.it>
2020-12-11 10:56:25 +0100

    Updated get_latest_transitions.sh

--- get_latest_transitions.sh
+++ get_latest_transitions.sh
@@ -1,9 +1,10 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1U0MzslsZU5XiHu93k-fwZgtHuJ3GXtJY' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1U0MzslsZU5XiHu93k-fwZgtHuJ3GXtJY" -O transitions_121034.npy && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1QjPEPI2MUCTlPQBr_laWIj6jHBvoOpz0' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1QjPEPI2MUCTlPQBr_laWIj6jHBvoOpz0" -O best_transitions.zip && rm -rf /tmp/cookies.txt
+unzip best_transitions.zip
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1K_Va61bDrF6RlEGgZysOVPg8kJXWs6fS' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1K_Va61bDrF6RlEGgZysOVPg8kJXWs6fS" -O VAE.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1_0t7P8OTa1u7CHZ5POv4GzTYeh_6WAg_' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1_0t7P8OTa1u7CHZ5POv4GzTYeh_6WAg_" -O VAE.zip && rm -rf /tmp/cookies.txt
 
 unzip VAE.zip
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1cC5cSaT7pmwQS3jCth2FrMgQes2go8JM' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1cC5cSaT7pmwQS3jCth2FrMgQes2go8JM" -O VAE_1080.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1eTjfLaqzDR4NqCpp1MngaAISjfBT6Hjq' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1eTjfLaqzDR4NqCpp1MngaAISjfBT6Hjq" -O VAE_1080.zip && rm -rf /tmp/cookies.txt
 
-unzip VAE_1080.zip
+#unzip VAE_1080.zip
